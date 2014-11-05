992db496a89656ab7e62c9999cc54b0fcf24db1b
Leonardo Rossi <leonardo.r@cern.ch>
2014-11-05 10:34:52 +0100

    Init: add bundles and deploy script
    * Adds some useful vim bundle as git submodules.
    
    * Adds a basic vimrc.
    
    * Adds a deploy script to install the vim environment.
    
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+DIR=`pwd`
+
+# Backup vim configuration
+BCK=$HOME/.vim.backup
+[ -e "$BCK" ] && echo "[Error] Backup directory already exists!" && exit 1
+[ ! -e "$BCK" ] && echo "[Backup] Start backup old vim environment." && mkdir $BCK
+#[ -e "$HOME/.vim" ] && echo "[Backup] backup ~/.vim" && mv ~/.vim $BCK/
+[ -e "$HOME/.vimrc" ] && echo "[Backup] backup ~/.vimrc" && mv ~/.vimrc $BCK/
+
+# Checkout plugins
+git submodule init
+git submodule update
+
+# Create directories
+[ ! -e "$HOME/.vim/bundle" ] && echo "[Create] ~/.vim/bundle" && mkdir -p ~/.vim/bundle
+
+# Link the current vimrc
+cd ~/
+ln -sf "$DIR/vimrc" ~/.vimrc
+# and the bundle
+for bundle in `ls $DIR/bundle/`; do
+  echo "[Link] bundle $bundle"
+  ln -sf $DIR/bundle/$bundle ~/.vim/bundle
+done
---
+++ vimrc
@@ -0,0 +1,4 @@
+"config the pathogen.vim runtime
+runtime bundle/vim-pathogen/autoload/pathogen.vim
+""active the pathogen plugin
+call pathogen#infect()
